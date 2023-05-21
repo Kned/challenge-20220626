9f8789e62a30c3cce04b6146fe5eb02825fad1e6
Eduardo Kettz Nunes <kettz_0@hotmail.com>
2023-05-21 18:24:41 -0300

    fixing route error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "/products", to: "products#index"
-  get "/products/:id", to: "products#index"
+  get "/products/:id", to: "products#show"
   root to: "application#root"
 end
